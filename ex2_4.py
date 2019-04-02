641e455fb86db4aab6f264af4473c59e1d9f3c1a
aytacyu <aytac125@gmail.com>
2019-04-02 23:27:12 +0100

    exercise 4-changes

---
+++ ex2_4.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Read in the "show_ip_int_brief.txt" file into your program using the
+.readlines() method.
+
+Obtain the list entry associated with the FastEthernet4 interface. You can
+just hard-code the index at this point since we haven't covered for-loops
+or regular expressions:
+
+Use the string .split() method to obtain both the IP address and the
+corresponding interface associated with the IP.
+
+Create a two element tuple from the result (intf_name, ip_address).
+
+Print that tuple to the screen.
+
+Use pycodestyle on this script. Get the warnings/errors to zero. You might need
+to 'pip install pycodestyle' on your computer (should be able to type this from
+the shell prompt). Alternatively, you can type:
+
+  'python -m pip install pycodestyle'.
+
+"""
+from __future__ import print_function, unicode_literals
+
+with open("show_ip_int_brief.txt") as f:
+    show_ip_int_brief = f.readlines()
+
+for line in show_ip_int_brief:
+    if "FastEthernet4" in line:
+        [intf_name, ip_address, *dummy] = line.strip().split()
+
+int_tupple = (intf_name, ip_address)
+print(int_tupple)
